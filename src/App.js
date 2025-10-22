68c83c0055927b286d9e97039d3311a6766fe7f0
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-10-22 23:08:33 +0530

    setup the react router and imported the different components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Navbar from './Components/Navbar';
+import AboutMe from './Components/AboutMe';
+import ContactMe from './Components/ContactMe';
+import Blog from './Components/Blog';
+import Projects from './Components/Projects';
+import Home from './Components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutMe />} />
+        <Route path="/contact" element={<ContactMe />} />
+        <Route path="/project" element={<Projects />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+      </Router>
     </div>
   );
 }
