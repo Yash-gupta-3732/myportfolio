347e5c3f8a219c26f95888025fa1c27e4cb9090b
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-10-26 11:17:26 +0530

    imported and used the footer component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import AboutMe from './Components/AboutMe';
 import ContactMe from './Components/ContactMe';
 import Skill from './Components/Skill';
 import Projects from './Components/Projects';
 import Home from './Components/Home';
+import Footer from './Components/Footer';
 import { Toaster } from "react-hot-toast";
+
 function App() {
   return (
-    <div>
-      <Router>
+    <Router>
+      {/* Page layout wrapper */}
+      <div className="flex flex-col min-h-screen bg-[#1e0e2b] text-white">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutMe />} />
-          <Route path="/contact" element={<ContactMe />} />
-          <Route path="/project" element={<Projects />} />
-          <Route path="/skill" element={<Skill />} />
-        </Routes>
-      </Router>
+
+        {/* Page content */}
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutMe />} />
+            <Route path="/skill" element={<Skill />} />
+            <Route path="/project" element={<Projects />} />
+            <Route path="/contact" element={<ContactMe />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+
+      {/* Toast notifications */}
       <Toaster
         position="top-right"
         toastOptions={{
@@ -42,7 +53,7 @@ function App() {
           },
         }}
       />
-    </div>
+    </Router>
   );
 }
 
